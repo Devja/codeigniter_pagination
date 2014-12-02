78083c77263fad112a1c2edea0d86453e818f5d8
Devja <dejva@naver.com>
2014-12-02 15:30:26 +0900

    기본 css 추가

---
+++ index.php
@@ -0,0 +1,19 @@
+<style>
+.paginate{padding:16px 0;text-align:center} 
+.paginate a,.paginate strong{display:inline-block;position:relative;_width /**/:17px;margin-right:1px;padding:2px 4px 3px;border:1px solid #fff;color:#000;font-family:Verdana;font-size:11px;font-weight:bold;line-height:normal;text-decoration:none} 
+.paginate strong{border:1px solid #e9e9e9;color:#f23219 !important} 
+.paginate .pre{margin-right:6px;padding:4px 6px 2px 14px;_padding-bottom:1px;background:url(http://static.naver.com/common/paginate/bu_pg3_l_off.gif) no-repeat 6px 5px !important;letter-spacing:-1px} 
+.paginate .next{margin-left:4px;padding:4px 14px 2px 6px;_padding-bottom:1px;background:url(http://static.naver.com/common/paginate/bu_pg3_r_off.gif) no-repeat 62px 5px !important;letter-spacing:-1px} 
+.paginate a.pre{background:url(http://static.naver.com/common/paginate/bu_pg3_l_on.gif) no-repeat 6px 5px !important} 
+.paginate a.next{background:url(http://static.naver.com/common/paginate/bu_pg3_r_on.gif) no-repeat 65px 5px !important} 
+.paginate .pre,.paginate .next{display:inline-block;position:relative;top:0;_top:-2px;_width /**/:75px;border:1px solid #e9e9e9;color:#ccc;font-family:'µ¸¿ò',Dotum;font-size:11px;line-height:normal} 
+.paginate a.pre,.paginate a.next{color:#565656} 
+.paginate a:hover{border:1px solid #e9e9e9;background-color:#f7f7f7 !important} 
+</style>
+
+
+
+
+
+
+<div style="text-align:center"><?=$this->pagination_custom->create_links();?></div>
